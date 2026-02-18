6666b051d3582411e7bff799234b1d6b25a0e0cd
Arham1243 <arham404khan@gmail.com>
2026-02-18 18:57:49 +0500

    Remove navigation menu, balance display, and agency profile from header and sidebar

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -13,42 +13,11 @@
                     alt="Andaleeb Travel Agency" />
             </a>
 
-            <nav class="mh-nav">
-                <ul class="mh-nav-list">
-                    <li><a href="">Flights</a></li>
-                    <li><a href="">Hotels</a></li>
-                    <li><a href="">Activities</a></li>
-                    <li><a href="">Travel Insurance </a></li>
-                    <li><a href="">Holidays</a></li>
-                </ul>
-            </nav>
-
-
-
             <div class="mh-user-area">
                 <a href="javascript:void(0);" class="mh-icon-link menu-btn" data-menu-button onclick="openSideBar()">
                     <i class="bx bx-menu"></i>
                 </a>
-                @if (Auth::check())
-                    <a href="#" class="mh-balance-pill">
-                        <div class="mh-balance-icon">
-                            <i class='bx bxs-wallet'></i>
-                        </div>
-                        <div class="mh-balance-info">
-                            <span class="mh-balance-label">Balance</span>
-                            <span class="mh-balance-amount">{!! formatPrice(Auth::user()->main_balance) !!}</span>
-                        </div>
-                    </a>
-                    <a href="{{ route('user.dashboard') }}" class="mh-agency-profile">
-                        <div class="mh-agency-logo">
-                            <img src="{{ asset(Auth::user()->avatar ?? 'frontend/assets/images/favicon.ico') }}" alt="Agency">
-                        </div>
-                        <div class="mh-agency-info">
-                            <span class="mh-agency-name">{{ Auth::user()->name }}</span>
-                            <span class="mh-agency-code">Agent Code: {{ Auth::user()->agent_code }}</span>
-                        </div>
-                    </a>
-                @else
+                @if (!Auth::check())
                     <a href="{{ route('auth.login') }}" class="themeBtn themeBtn--primary">Login</a>
                 @endif
             </div>
@@ -62,13 +31,6 @@
         <img src="{{ isset($config['SITE_LOGO']) ? asset($config['SITE_LOGO']) : asset('frontend/assets/images/logo.webp') }}"
             alt="Andaleeb Travel Agency" />
     </a>
-    <ul class="sideBar__nav">
-        <li><a href="">Flights</a></li>
-        <li><a href="">Hotels</a></li>
-        <li><a href="">Activities</a></li>
-        <li><a href="">Travel Insurance </a></li>
-        <li><a href="">Holidays</a></li>
-    </ul>
     @if (!Auth::check())
         <a href="{{ route('auth.login') }}" class="themeBtn themeBtn--primary">Login</a>
     @endif
