ab87d6049e2f3715f6ab77ccd2ec82b5583f25bd
Arham1243 <arham404khan@gmail.com>
2026-02-18 18:52:09 +0500

    Replace Inquiry model with B2bInquiry in InquiryController

--- app/Http/Controllers/Admin/InquiryController.php
+++ app/Http/Controllers/Admin/InquiryController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Inquiry;
+use App\Models\B2bInquiry;
 
 class InquiryController extends Controller
 {
     public function index()
     {
-        $contacts = Inquiry::latest()->get();
+        $contacts = B2bInquiry::latest()->get();
         return view('admin.inquiries.list', compact('contacts'));
     }
 
-    public function destroy(Inquiry $contact)
+    public function destroy(B2bInquiry $contact)
     {
         $contact->delete();
         return redirect()->route('admin.inquiries.index')->with('notify_success', 'Contact deleted successfully!');
